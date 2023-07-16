1a7e0b69cd0bffdd3e3ed8658167c79f5ce015d5
ShadeeCat <assulema@gmail.com>
2023-07-16 13:11:51 +0300

    added change background color input and logic

--- src/components/RightSide_favoritePlayerList.tsx
+++ src/components/RightSide_favoritePlayerList.tsx
@@ -3,24 +3,41 @@ import React, { useState } from "react";
 import { PlayerListTypes } from "../interfaces/PlayerListTypes";
 
 export interface FavoritePlayerProps {
-    favoritePlayers: PlayerListTypes[],
-    onRemoveFavorite: (player: PlayerListTypes) => void,
+	favoritePlayers: PlayerListTypes[],
+	onRemoveFavorite: (player: PlayerListTypes) => void,
 }
 
 export const FavoritePlayersList: React.FC<FavoritePlayerProps> = ({ favoritePlayers, onRemoveFavorite }) => {
 
-    const handleRemoveFavoriteClick = (player: PlayerListTypes) => {
-        onRemoveFavorite(player)
-    }
+	const [backgroundColor, setBackgroundColor] = useState<string>("")
 
-    return <div>
-        <ul>
-            { favoritePlayers.map(player => (
-                <li key={ player.id }>
-                    { player.first_name } { player.last_name } - { player.position } { player.team.full_name }
-                    <button onClick={ () => handleRemoveFavoriteClick(player) } >Remove Player</button>
-                </li>
-            )) }
-        </ul>
-    </div>
+	const handleRemoveFavoriteClick = (player: PlayerListTypes) => {
+		onRemoveFavorite(player)
+	}
+
+	const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setBackgroundColor(event.target.value)
+	}
+
+	const listStyle = {
+		backgroundColor: backgroundColor || "transparent"
+	}
+
+	return <div style={ listStyle }>
+		<label htmlFor="color-input">background color</label>
+		<input
+			id="color-input"
+			type="color"
+			value={ backgroundColor }
+			onChange={ handleColorChange }
+		/>
+		<ul>
+			{ favoritePlayers.map(player => (
+				<li key={ player.id }>
+					{ player.first_name } { player.last_name } - { player.position } { player.team.full_name }
+					<button onClick={ () => handleRemoveFavoriteClick(player) } >Remove Player</button>
+				</li>
+			)) }
+		</ul>
+	</div>
 }
